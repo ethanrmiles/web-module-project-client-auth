f3d9acc8276cab68ad09fd2be9d3520dcaba55b1
Ethan Miles <ethanmilescs@gmail.com>
2022-03-29 15:10:24 -0500

    friends are fetched from api when the friendsList component mounts, and the array of friends is stored in state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, {  useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LoginForm from './components/loginForm';
 import FriendList from './components/friendList';
 import AddFriend from './components/addFriend';
 import { NavLink } from 'react-router-dom';
 import axios from 'axios';
+import axiosWithAuth from './axios'
 
 const loginURL = 'http://localhost:9000/api/login'
+const friendURL = 'http://localhost:9000/api/friends'
 
 function App() {
+const [friends, setFriends] = useState([])
+
 
   const login = ({ username, password }) => {
     axios.post(loginURL, { username, password  })
     .then(res => {
       const token = res.data.token
       window.localStorage.setItem('token', token)
+    })
+    .catch(err => {
+      debugger
+    })
+  }
+
+  const logout = () => {
+    window.localStorage.removeItem('token')
+  }
+
+  const getFriends = () => {
+    axiosWithAuth().get(friendURL)
+    .then(res => {
+      setFriends(res.data)
     })
     .catch(err => {
       debugger
@@ -30,12 +48,13 @@ function App() {
         <NavLink id='loginScreen' to='/login'>Login</NavLink>
         <NavLink id='friendList' to='/friends'>Friends List</NavLink>
         <NavLink id='addFriend' to='/friends/add'>Add Friend</NavLink>
+        <button id='logout' onClick={logout}>Logout</button>
       </nav>
       <Route path='/login'>
         <LoginForm login={login} />
       </Route>
       <Route path='/friends'>
-        <FriendList />
+        <FriendList getFriends={getFriends} />
       </Route>
       <Route path='/friends/add' >
         <AddFriend />
--- src/components/friendList.js
+++ src/components/friendList.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function FriendList (props) {
+    const { getFriends } = props
+    
+    useEffect(() => {
+        getFriends()
+    }, [])
+
     return (
-        <h2>Friend List:</h2>
+      <div className="friendList">
+            <h2>Friend List:</h2>
+            <p>Friend Name</p> 
+            <p> Friend email</p>
+      </div>
     )
 }
